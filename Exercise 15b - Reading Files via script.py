cbf4e56dc6c6f8061d6d629ac0ba3b80f719c618
CoderKola <119469578+CoderKola@users.noreply.github.com>
2023-03-23 23:02:00 -0400

    added another example

--- Exercise 15b - Reading Files via script.py
+++ Exercise 15b - Reading Files via script.py
@@ -1,5 +1,31 @@
+# This one does not require import module
+
+# We are sending a string that informs the user to input the file name.
 print("Please type your file name: ")
+
+# This line of code gives the prompt in the terminal for where to input the code
+# we use the input() function and store the user's input into the variable called
+    # 'file_name'
 file_name = input("> ")
 
+
+# we then name a variable called 'txt_file' and store the function
+# open() function with the parameter of 
+    # file_name
+    # which was what the user just gave us
+    # it uses the full name along with the type of file extension it is
+    # (i.e .txt)
 txt_file = open(file_name)
+
+# in the end, we print whatever was in that text file using the print function
+# inside that print function is a the parameter we gave it to read
+# we are commanding the txt_file.read() to be read
 print(txt_file.read())
+
+
+# Example 2
+# print("Enter your file name: ")
+# file_name_prompt = input("> ")
+
+# open_file  = open(file_name_prompt)
+# print(open_file.read())
